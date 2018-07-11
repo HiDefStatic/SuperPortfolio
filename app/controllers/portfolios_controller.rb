54d43755e8983c76757ae5c465a2d6d9f3dc86f2
HiDefStatic <timothy.s.deel@gmail.com>
2018-07-10 18:33:46 -0700

    creates destroy action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,4 +39,12 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio item has been removed.' }
+    end
+  end
+
 end
